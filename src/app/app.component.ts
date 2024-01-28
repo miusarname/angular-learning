e59c80d12ee9c94f86e602f7870e533385894051
miusarname <oscarm.alvarezg@gmail.com>
2024-01-27 23:11:12 -0500

    feat: :sparkles: new methods

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,27 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { UserLogibComponent } from './user-logib/user-logib.component';
+import { CommentsComponent } from './comments/comments.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,UserLogibComponent],
+  imports: [RouterOutlet, UserLogibComponent, CommentsComponent],
   templateUrl: './app.component.html',
   // template:`
   // <h1>Hola mundo,desde {{city}}</h1>
   // <app-user-logib/>
   // `,
-  styles:`
+  styles: `
   h1{
     font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
   }
-  `
+
+.container{
+  max-width: 50%;
+}
+  `,
 })
 export class AppComponent {
-  city="Bucaramanga";
+  city = 'Bucaramanga';
 }
